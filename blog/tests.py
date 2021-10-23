27f09722028d853e295f6a27e94e70329ed76023
jenadiusnicholaus <ucode888@gmail.com>
2021-10-23 17:11:32 +0300

    looking github commit

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from . models import Post
 
-# Create your tests here.
+
+class ModelTest(TestCase):
+
+    def setUp(self):
+        self.blog = Post.objects.create(title='Django', author='Django', slug='django')
+
+    def test_post_model(self):
+        data = self.blog
+        self.assertTrue(isinstance(data, Post))
+        self.assertEqual(str(data), 'Django')
